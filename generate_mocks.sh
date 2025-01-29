b106aff6ea769138892105ce5ed6f5c66d64df48
Alexander Trakhimenok <alex@trakhimenok.com>
2025-01-29 09:12:13 +0000

    fix: added generate_mocks.sh and fixed folder name

---
+++ generate_mocks.sh
@@ -0,0 +1,5 @@
+mockgen github.com/bots-go-framework/bots-fw/botsfw WebhookContext > mock_botsfw/webhook_context.go
+mockgen github.com/bots-go-framework/bots-fw/botsfw WebhookResponder > mock_botsfw/webook_responder.go
+mockgen github.com/bots-go-framework/bots-fw/botsfw BotHost > mock_botsfw/bot_host.go
+mockgen github.com/bots-go-framework/bots-fw/botsfw BotPlatform > mock_botsfw/bot_platform.go
+mockgen github.com/bots-go-framework/bots-fw/botinput WebhookInput > mock_botinput/webook_input.go
